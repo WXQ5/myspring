d6346e2fbd5c8183b46899c74525b86ab2b60aef
落凤 <W563604287@163.com>
2020-12-17 15:55:56 +0800

    手写框架

---
+++ src/com/myspring/core/ClassPathXmlApplicationContext.java
@@ -0,0 +1,118 @@
+package com.myspring.core;
+
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+import org.xmlpull.v1.XmlPullParserFactory;
+
+import java.io.InputStream;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
+
+public class ClassPathXmlApplicationContext implements ApplicationContext{
+    private String configFileName;
+
+    public ClassPathXmlApplicationContext(String configFileName) {
+        this.configFileName = configFileName;
+    }
+
+    public ClassPathXmlApplicationContext() {
+        this.configFileName = "applicationContext.xml";
+    }
+    //1--解析xml文件
+    public Map<String,EntityBean> springXmlParser() throws Exception {
+       //创建解析器
+        XmlPullParser pullParser = XmlPullParserFactory.newInstance().newPullParser();
+        InputStream in = ClassPathXmlApplicationContext.class.getClassLoader().getResourceAsStream(configFileName);
+        pullParser.setInput(in,"utf-8");
+        int eventType = pullParser.getEventType();
+        Map<String,EntityBean> beans = null;
+        EntityBean bean = null;
+        while (eventType != XmlPullParser.END_DOCUMENT) {
+            switch (eventType) {
+                case XmlPullParser.START_DOCUMENT:
+                    beans = new HashMap<String, EntityBean>();
+                    break;
+                case XmlPullParser.START_TAG:
+                    if("bean".equals(pullParser.getName())){
+                        bean = new EntityBean();
+                        bean.setId(pullParser.getAttributeValue(null,"id"));
+                        bean.setClassName(pullParser.getAttributeValue(null,"class"));
+                    }
+                    if("property".equals(pullParser.getName())){
+                        String attrName = pullParser.getAttributeValue(null,"name");
+                        String vattrVal = pullParser.getAttributeValue(null,"value");
+                        bean.getProps().put(attrName,vattrVal);
+                    }
+                    break;
+                case XmlPullParser.END_TAG:
+                    beans.put(bean.getId(),bean);
+                    break;
+
+            }
+            eventType= pullParser.next();
+        }
+
+        return beans;
+    }
+
+/*    public static void main(String[] args) throws Exception {
+        ClassPathXmlApplicationContext cpxs = new ClassPathXmlApplicationContext();
+        cpxs.springXmlParser();
+        Map<String,EntityBean> beans =cpxs.springXmlParser();
+        for(Map.Entry<String,EntityBean> entry:beans.entrySet()){
+            System.out.println(entry.getKey());
+            System.out.println(entry.getValue());
+            System.out.println("*************");
+        }
+    }*/
+
+    //2--根据解析来的信息，通过反射返程对象组装assemble
+    public Map<String,Object> getIOC(Map<String ,EntityBean> beansInfo) throws Exception {
+        Map<String,Object> results = new HashMap<String,Object>();
+        for(Map.Entry<String,EntityBean> beanInfo:beansInfo.entrySet()){
+            String resultId = beanInfo.getKey();
+            EntityBean bean = beanInfo.getValue();
+            String className = bean.getClassName();
+            Map<String,String> props = bean.getProps();
+
+            //反射--输入字符串，返回对象
+            Class clazz = Class.forName(className);
+            Object obj = clazz.newInstance();
+            for(Map.Entry<String,String> prop:props.entrySet()){
+                String propName = prop.getKey();
+                String propValue = prop.getValue();
+                StringBuilder buffer = new StringBuilder("set");
+                buffer.append(propName.substring(0,1).toUpperCase());
+                buffer.append(propName.substring(1));
+
+                String setterMethodName = buffer.toString();
+                Field field = clazz.getDeclaredField(propName);
+                Method setMethod = clazz.getDeclaredMethod(setterMethodName,field.getType());
+                if("int".equals(field.getType().getName())){
+                    setMethod.invoke(obj,Integer.parseInt(propValue));
+                }else if("java.lang.String".equals(field.getType().getName())){
+                    setMethod.invoke(obj,propValue);
+                }
+                results.put(resultId,obj);
+            }
+        }
+
+        return  results;
+    }
+    @Override
+    public Object getBean(String beanID) {
+        Object result = null;
+
+        try {
+            Map<String,EntityBean> beansInfo = springXmlParser();
+            Map<String, Object> ioc = getIOC(beansInfo);
+            result = ioc.get(beanID);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return result;
+    }
+}
---
+++ src/com/myspring/core/EntityBean.java
@@ -0,0 +1,47 @@
+package com.myspring.core;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class EntityBean {
+    private String id;
+    private String className;
+    private Map<String,String> props;
+
+    public EntityBean() {
+        props = new HashMap<String,String >();
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getClassName() {
+        return className;
+    }
+
+    public void setClassName(String className) {
+        this.className = className;
+    }
+
+    public Map<String, String> getProps() {
+        return props;
+    }
+
+    public void setProps(Map<String, String> props) {
+        this.props = props;
+    }
+
+    @Override
+    public String toString() {
+        return "EntityBean{" +
+                "id='" + id + '\'' +
+                ", className='" + className + '\'' +
+                ", props=" + props +
+                '}';
+    }
+}
